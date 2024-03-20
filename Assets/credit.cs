6f87847a1d22fb2efcca4db83610f063ccdb373c
akil <akil.abdulrahim@gmail.com>
2024-03-20 10:44:28 +0200

    tiles

---
+++ Assets/credit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class credit : MonoBehaviour
+{
+    [SerializeField] private GameObject credits;
+
+    public void Credit()
+    {
+        credits.SetActive(!credits.active);
+    }
+
+}
