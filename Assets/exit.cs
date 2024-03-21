841e56084881e0fdc321610c54bd0bd930648dd4
akil <akil.abdulrahim@gmail.com>
2024-03-21 11:50:28 +0200

    ok

--- Assets/exit.cs
+++ Assets/exit.cs
@@ -5,14 +5,8 @@ using UnityEngine;
 public class exit : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    public void QuiteGame()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Application.Quit();
     }
 }
